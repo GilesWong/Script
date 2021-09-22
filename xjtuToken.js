2ee71b3299f5e48b064cf98298636c6fbb13bdd0
Giles Wong <iwg@live.com>
2021-09-22 23:51:20 +0800

    Update xjtuToken.js

--- xjtuToken.js
+++ xjtuToken.js
@@ -1,5 +1,6 @@
 var responseBody = $response.body
 console.log(responseBody)
-var xjtuToken = responseBody['data'][personToken]
+var xjtuToken = responseBody['data']['personToken']
 console.log(xjtuToken)
+if (xjtuToken) {$prefs.setValueForKey(xjtuToken, 'xjtuToken')}
 $done()
